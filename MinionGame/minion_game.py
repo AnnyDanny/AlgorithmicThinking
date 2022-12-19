c5e7ee8cdbc660f00dd96098778e9a411de43113
Hanna Danylova <hanna.danylova@avensia.com>
2022-12-19 12:08:27 +0100

    Finish basic minion game functionality

---
+++ MinionGame/minion_game.py
@@ -0,0 +1,27 @@
+def minion_game(string):
+    scoreConconants = 0
+    scoreVowels = 0
+
+    for substrLen in range(1, len(string) + 1):
+        for startPos in range(len(string)):
+            if substrLen + startPos > len(string):
+                break
+            element = string[startPos:substrLen + startPos]
+            if element[0].lower() in "aeiou":
+                scoreVowels += 1
+            else:
+                scoreConconants += 1
+
+
+    if scoreVowels > scoreConconants:
+        print("Kevin", scoreVowels)
+    elif scoreVowels == scoreConconants:
+        print("Draw")
+    else:
+        print("Stuart", scoreConconants)
+
+
+
+if __name__ == '__main__':
+    s = "BANANANAAAS"
+    minion_game(s)
